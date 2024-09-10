4075289ad4e014f7c65bd7477a2e34a394dc8d2c
Fanceir <fx_official@outlook.com>
2024-09-10 21:55:30 +0800

    update

--- sidebars.ts
+++ sidebars.ts
@@ -3,22 +3,22 @@ import type { SidebarsConfig } from '@docusaurus/plugin-content-docs'
 const sidebars: SidebarsConfig = {
   skill: [
     'skill/introduction',
-    {
-      label: '代码规范',
-      type: 'category',
-      link: {
-        type: 'doc',
-        id: 'skill/code-specification/code-specification-guides',
-      },
-      items: [
-        'skill/code-specification/eslint',
-        'skill/code-specification/prettier',
-        'skill/code-specification/stylelint',
-        'skill/code-specification/editorconfig',
-        'skill/code-specification/husky',
-        'skill/code-specification/npmrc',
-      ],
-    },
+    // {
+    //   label: '代码规范',
+    //   type: 'category',
+    //   link: {
+    //     type: 'doc',
+    //     id: 'skill/code-specification/code-specification-guides',
+    //   },
+    //   items: [
+    //     'skill/code-specification/eslint',
+    //     'skill/code-specification/prettier',
+    //     'skill/code-specification/stylelint',
+    //     'skill/code-specification/editorconfig',
+    //     'skill/code-specification/husky',
+    //     'skill/code-specification/npmrc',
+    //   ],
+    // },
     {
       label: 'Web',
       type: 'category',
