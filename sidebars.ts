4a829c96b5964b9dadcf4cbf5e535ee663a17822
Fanceir <fx_official@outlook.com>
2024-09-10 21:49:48 +0800

    update

--- sidebars.ts
+++ sidebars.ts
@@ -145,20 +145,20 @@ const sidebars: SidebarsConfig = {
         },
       ],
     },
-    // {
-    //   label: '算法',
-    //   type: 'category',
-    //   link: {
-    //     type: 'doc',
-    //     id: 'skill/algorithm/algorithm-introduction',
-    //   },
-    //   items: [
-    //     'skill/algorithm/two-sum',
-    //     'skill/algorithm/three-sum',
-    //     'skill/algorithm/sliding-window',
-    //     'skill/algorithm/double-pointer',
-    //   ],
-    // },
+    {
+      label: '算法',
+      type: 'category',
+      link: {
+        type: 'doc',
+        id: 'skill/algorithm/algorithm-introduction',
+      },
+      items: [
+        'skill/algorithm/two-sum',
+        'skill/algorithm/three-sum',
+        'skill/algorithm/sliding-window',
+        'skill/algorithm/double-pointer',
+      ],
+    },
     // {
     //   label: 'Docker',
     //   type: 'category',
