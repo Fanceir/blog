cfad262cf142e0fbab4d641e49dc6f85c254676a
Fanceir <fx_official@outlook.com>
2024-10-31 16:11:12 +0800

    feat: 万圣节博客小彩蛋

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,20 +1,32 @@
-import useDocusaurusContext from '@docusaurus/useDocusaurusContext'
-import Layout from '@theme/Layout'
-import BlogSection from '../components/landing/BlogSection'
-import FeaturesSection from '../components/landing/FeaturesSection'
-import Hero from '../components/landing/Hero'
-import ProjectSection from '../components/landing/ProjectSection'
-import Particles from '../components/magicui/particles'
+import useDocusaurusContext from '@docusaurus/useDocusaurusContext';
+import Layout from '@theme/Layout';
+import Lottie from 'lottie-react';
+import animationData from '../../static/halloween.json';
+import BlogSection from '../components/landing/BlogSection';
+import FeaturesSection from '../components/landing/FeaturesSection';
+import Hero from '../components/landing/Hero';
+import ProjectSection from '../components/landing/ProjectSection';
+import Particles from '../components/magicui/particles';
 
 export default function Home() {
   const {
     siteConfig: { customFields, tagline },
-  } = useDocusaurusContext()
-  const { description } = customFields as { description: string }
+  } = useDocusaurusContext();
+  const { description } = customFields as { description: string };
 
   return (
     <Layout title={tagline} description={description}>
       <main>
+        {/* 左上角 Lottie 动画 */}
+        <div style={{ position: 'absolute', top: '50px', left: '50px', zIndex: 10 }}>
+          <Lottie
+            animationData={animationData}
+            loop
+            autoplay
+            style={{ width: '150px', height: '150px' }} // 控制动画大小
+          />
+        </div>
+
         <Hero />
         <Particles className="absolute inset-0" quantity={100} ease={80} color={'#ffffff'} refresh />
 
@@ -26,10 +38,10 @@ export default function Home() {
           </div>
           <div
             className="-z-50 absolute inset-0 bg-grid-slate-50 [mask-image:linear-gradient(0deg,#fff,rgba(255,255,255,0.3))] dark:bg-grid-slate-700/25 dark:[mask-image:linear-gradient(0deg,rgba(255,255,255,0.1),rgba(255,255,255,0.5))]"
-            style={{ backgroundPosition: '10px 10px;' }}
+            style={{ backgroundPosition: '10px 10px' }}
           />
         </div>
       </main>
     </Layout>
-  )
+  );
 }
